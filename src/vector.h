4ee79627338a44f5f3c84935bde57f846f5e4d4f
Daniel Hauser <daniel.hauser@liwest.at>
2013-07-25 10:46:45 +0200

    Fixed VecDot

--- src/vector.h
+++ src/vector.h
@@ -16,7 +16,7 @@ typedef vec_t vec3_t[3];
 #define VecCopy(v, o) ((o)[0] = (v)[0], (o)[1] = (v)[1], (o)[2] = (v)[2])
 #define VecClear(v) ((v)[0] = (v)[1] = (v)[2] = 0.0f)
 #define VecNegate(v, o) ((o)[0] = -(v)[0], (o)[1] = -(v)[1], (o)[2] = -(v)[2])
-#define VecDot(v) ((v)[0]*(v)[0] + (v)[1]*(v)[1] + (v)[2]*(v)[2])
+#define VecDot(a, b) ((a)[0]*(b)[0] + (a)[1]*(b)[1] + (a)[2]*(b)[2])
 #define VecCross(a, b, o) ((o)[0] = (a)[1]*(b)[2] - (a)[2]*(b)[1], (o)[1] = (a)[2]*(b)[0] - (a)[0]*(b)[2], (o)[2] = (a)[0]*(b)[1] - (a)[1]*(b)[0])
 #define VecScale(v, s, o) ((o)[0] = (v)[0] * (s), (o)[1] = (v)[1] * (s), (o)[2] = (v)[2] * (s))
 #define VecCmp(a, b) ((a)[0] == (b)[0] && (a)[1] == (b)[1] && (a)[2] == (b)[2])
